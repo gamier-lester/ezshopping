98df0f163a261314e8c709654a9b1e38d0365936
Lester Jan Gamier <gamier.lester@gmail.com>
2019-07-19 02:31:40 +0800

    debug

--- controllers/process.add-orders.php
+++ controllers/process.add-orders.php
@@ -3,6 +3,23 @@
 	require_once("library.php");
 	require_once("../config/path.php");
 
+	$query = "INSERT INTO ecom_order (id, transaction_id, order_date, order_update, order_status, order_type, order_amount, user_id, item_id, item_price, item_quantity) VALUES ";
+		$array_count = 1;
+		$length = count($_SESSION['user_cart']['user_orders']);
+		foreach($_SESSION['user_cart']['user_orders'] as $key => $value) {
+			$item_id = $key;
+			$item_price = $value['item_price'];
+			$item_quantity = $value['order_quantity'];
+			$order_amount = bcdiv(strval($item_price * $item_quantity), '1', 2);
+			$query .= "(NULL, '$last_id', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '1', '2', '$order_amount', '$user_id', '$item_id', '$item_price', '$item_quantity')";
+			if ($array_count != $length) {
+				$query .= ', ';
+			}
+		}
+
+		echo $query;
+		exit;
+
 	if (!isset($_SESSION['user_credentials'])) {
 		$_SESSION["error_login"] = "You must login first!";
 		header("Location: ".ROOT_URL."/views/member/login.php");
