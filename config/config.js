ca7848b8895b93df99a8d240216322cece8779b6
Lester Jan Gamier <gamier.lester@gmail.com>
2019-07-22 21:22:28 +0800

    heroku test-deploy

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ export default
 	"production" : true,
 	"environment" : "development",
 	"projectUrl" : {
-		"production" : "",
+		"production" : "https://ez-shopping.herokuapp.com",
 		"development" : "http://localhost:8080/e-commerce"
 	},
 	"defaultImage" : "https://firebasestorage.googleapis.com/v0/b/ez-shopping-11c7a.appspot.com/o/images%2F1563375344592?alt=media&token=24541b92-f339-4398-86a8-1c3bb32c7db2"
