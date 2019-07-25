ac30e91a443852c804747d6197099b94bc71a8ea
Lester Gamier <gamier.lester@gmail.com>
2019-07-26 01:53:27 +0800

    very wrong link on config sorry

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ export default
 		"development" : "http://localhost/e-commerce"
 	},
 	"projectApi" : {
-		"production" : "https://e-z-shopping.000webhost.com",
+		"production" : "https://e-z-shopping.000webhostapp.com/",
 		"development" : "http://localhost/phpserver/ez-shopping-api"
 	},
 	"defaultImage" : "https://firebasestorage.googleapis.com/v0/b/ez-shopping-11c7a.appspot.com/o/images%2F1563375344592?alt=media&token=24541b92-f339-4398-86a8-1c3bb32c7db2"
